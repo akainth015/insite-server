5da879a26958f3ea8028fe15d52256bd03661596
ksakib <ksakib@ucsc.edu>
2022-10-16 22:17:54 -0400

    basic server setupp

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask
+from flask_socketio import SocketIO, send, emit
+from flask_cors import CORS
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+# socketio = SocketIO(app)
+CORS(app,resources={r"/*":{"origins":"*"}})
+socketio = SocketIO(app,cors_allowed_origins="*")
 
+
+@socketio.on("connect")
+def connected():
+    print("client connected")
+    emit("logs", {"data": "Connected"})
+
+@socketio.on("new_node")
+def new_node_added():
+    emit("logs", {"data": "new_node"})
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True,port=5001)
+"""
 @app.route("/")
 def hello():
   return "Hello World!"
 
 if __name__ == "__main__":
   app.run()
+"""
