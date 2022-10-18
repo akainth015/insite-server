f4d702004b832345a1f7d77516c297436f51dd5f
ksakib <ksakib@ucsc.edu>
2022-10-17 20:04:27 -0400

    minor fix

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 # socketio = SocketIO(app)
-CORS(app,resources={r"/*":{"origins":"*"}})
-socketio = SocketIO(app,cors_allowed_origins="*")
+CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @socketio.on("connect")
@@ -13,16 +13,19 @@ def connected():
     print("client connected")
     emit("logs", {"data": "Connected"})
 
+
 @socketio.on("new_node")
 def new_node_added():
     emit("logs", {"data": "new_node"})
 
+
 @socketio.on("default_node")
 def default_node():
-    emit("logs", {"data": "default_node"})    
+    emit("logs", {"data": "default_node"})
+
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True,port=5001)
+    socketio.run(app, debug=True, port=3000)
 """
 @app.route("/")
 def hello():
