5d041ae99d54c11029372f9b0bc8acf4426138ad
billzhangsc <billzhangsc@gmail.com>
2022-10-22 14:57:22 -0700

    sending and receiving works

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-@socketio.on("linear_regression")
+
+@socketio.on("linear")
 def handle_linear_regression(json):
-    print('received json: ' + str(json))
-
+    print("Received a json " + str(type(json)))
+    emit("linear", json, broadcast=False)
 
 
 if __name__ == '__main__':
