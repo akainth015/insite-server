06c99e6ee98215c295ea7cbfce49003814b1580b
Bill Zhang <billzhangsc@gmail.com>
2022-11-16 16:21:15 -0800

    Finalizing linear api route

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_socketio import SocketIO, emit
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from Models.LinearRegression import LinearRegressionTrainer
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, accuracy_score
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -21,31 +22,38 @@ def handle_linear_regression(nodeId, features, labels, feature_names, label_name
     # Convert the labels to numpy array
     labels = np.array(labels)
 
-    #Split the data into training and validation data
+    # Split the data into training and validation data
     x_train, x_val, y_train, y_val = train_test_split(features_df, labels, test_size=0.2)
 
-    #Create a LinearRegressionTrainer object
-    trainer = LinearRegressionTrainer(len(features_df.columns) - 1,
-                                        learning_rate=0.5,
-                                        num_epochs=10000)
+    # Create a linear regression model and fit
+    model = LinearRegression().fit(x_train, y_train)
 
+    # Get the predictions on the training and validation data
+    y_train_pred = model.predict(x_train)
+    y_val_pred = model.predict(x_val)
 
+    # Get the training and validation accuracy
+    train_accuracy = accuracy_score(y_train, y_train_pred)
+    val_accuracy = accuracy_score(y_val, y_val_pred)
+    
 
-    #Train
-    trainer.train(x_train, y_train, x_val, y_val)
-    val_loss = trainer.evaluate(x_val, y_val)
-    train_loss = trainer.evaluate(x_train, y_train)
+    # Get the training and validation loss
+    train_loss = mean_squared_error(y_train, y_train_pred)
+    val_loss = mean_squared_error(y_val, y_val_pred)
+
 
     #Store train and val losses in result dictionary
     result = {
         "train_loss": train_loss,
-        "val_loss": val_loss
+        "val_loss": val_loss,
+        "train_accuracy": train_accuracy,
+        "val_accuracy": val_accuracy
     }
 
     #Store the model in the global models dictionary
     models[nodeId] = trainer
 
-    emit("linear", result, broadcast=False)
+    emit("linear", (nodeId, result), broadcast=False)
 
 
 if __name__ == '__main__':
