8370285aa960fba1b4d295bf66e2b83b9fa060b2
Bill Zhang <billzhangsc@gmail.com>
2022-11-28 20:30:15 -0800

    Fun!

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def generate(nodeId, inputObj):
         engine="text-davinci-003",
         prompt=inputObj['text'],
         temperature=0.5,
-        max_tokens=60,
+        max_tokens=500,
         top_p=1,
         frequency_penalty=0.5,
         presence_penalty=0,
